435a17a43196316118b1b1bc1386b47486d2ea96
Chance Hudson <jchancehud@gmail.com>
2019-07-10 16:27:36 +0200

    remove unused docker content

--- credentials.example.js
+++
@@ -1,5 +0,0 @@
-Object.assign(process.env, {
-  SENDER_EMAIL: 'emailaccount@provider.com',
-  SENDER_PASSWORD: 'email_account_password',
-  RECEIVER_EMAILS: 'test@email.com,anotheremail@email.com',
-})

15c9e95e6ae6412674f80adeaa98514155491e79
Chance Hudson <jchancehud@gmail.com>
2019-07-10 16:27:07 +0200

    now json file, refactor index, remove ssl, remove credential loading

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-const fs = require('fs')
-const https = require('https')
-const http = require('http')
 const nodemailer = require('nodemailer')
 const express = require('express')
-const path = require('path')
 const validator = require('email-validator')
-
-// If we have local credentials load them
-if (fs.existsSync('./credentials.js')) {
-  require('./credentials')
-}
 
 if (!process.env.RECEIVER_EMAILS) {
   console.log('No receiver emails specified')
@@ -35,13 +26,6 @@ app.use((_, res, next) => {
   res.set('Access-Control-Allow-Headers', 'content-type')
   next()
 })
-
-const sslPath = '/ssl'
-
-const server = fs.existsSync(sslPath) ? https.createServer({
-  key: fs.readFileSync(path.join(sslPath, 'privkey.pem')),
-  cert: fs.readFileSync(path.join(sslPath, 'fullchain.pem')),
-}, app) : http.createServer(app)
 
 app.post('/send', async (req, res) => {
   try {
@@ -69,9 +53,4 @@ Comments: ${req.body.comments}`,
   }
 })
 
-app.get('/ping', (req, res) => res.send('pong'))
-
-// Bind to 443 at the vm abstraction level
-server.listen(4000, () => {
-  console.log('Listening for emails at', receivers.join(','))
-})
+module.exports = app
